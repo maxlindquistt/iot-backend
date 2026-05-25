cf035b05bfaf4fe7baff4dfdfaa690c508cefef0
Max Lindquist <max.lindquist312@gmail.com>
2026-05-25 13:02:53 +0200

    create mqtt handler

---
+++ src/mqttHandler.js
@@ -0,0 +1,32 @@
+const mqtt = require('mqtt');
+const { insertReading } = require('./db');
+
+const MQTT_BROKER = 'mqtt://broker.hivemq.com';
+const TOPIC_SENSOR = 'lnu/iot/ml227cu/sensor';
+const TOPIC_COMMAND = 'lnu/iot/ml227cu/command/led';
+
+let client;
+
+function initMqtt(io) {
+    client = mqtt.connect(MQTT_BROKER);
+
+    client.on('connect', () => {
+        console.log('Connected to MQTT broker');
+        client.subscribe(TOPIC_SENSOR);
+    });
+
+    client.on('message', (topic, message) => {
+        if (topic === TOPIC_SENSOR) {
+            const data = JSON.parse(message.toString());
+            console.log('Received:', data);
+            insertReading(data.value, data.humidity, data.timestamp);
+            io.emit('sensor_data', data);
+        }
+    });
+}
+
+function sendCommand(state) {
+    client.publish(TOPIC_COMMAND, JSON.stringify({ state }));
+}
+
+module.exports = { initMqtt, sendCommand };
