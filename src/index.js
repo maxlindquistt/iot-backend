63a2d99717ccd934e1420fd29e08d7bccc3f5d5c
Max Lindquist <max.lindquist312@gmail.com>
2026-05-25 13:09:25 +0200

    feat: implement initial server setup with Express and Socket.io

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import http from 'http';
+import { Server } from 'socket.io';
+import { initMqtt } from './mqttHandler.js';
+import routes from './routes/routes.js';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: '*' } });
+
+app.use(cors());
+app.use(express.json());
+app.use('/api', routes);
+
+initMqtt(io);
+
+server.listen(3000, () => {
+    console.log('Backend running on http://localhost:3000');
+});
